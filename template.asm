1b3976ebe324b145d35fee0c23fc92a2d71158e1
Harshvardhan Singh <singhhar@oregonstate.edu>
2018-02-18 19:34:33 -0800

    Added composite number checker & printer

--- template.asm
+++ template.asm
@@ -1,146 +1,156 @@
 TITLE Program Template     (template.asm)
 
-; Author:
-; Course / Project ID                 Date:
-; Description:
+; Author:			   Harshvardhan Singh
+; Course / Project ID: CS 271                 Date: 1/26/2018
+; Description: Program 2
 
 INCLUDE Irvine32.inc
 
-
-
-; (insert constant definitions here)
+;.386
+;.model flat,stdcall
+;.stack 4096
+ExitProcess proto,dwExitCode:dword
 
 .data
-header			BYTE	"Elementary Arithmetic		by Harshvardhan Singh",0
-_instructions   BYTE	"Enter 2 numbers, and I'll show you the sum difference, product, quotient, and remainder.",0
-str1			BYTE    "Enter first number ",0
-str2			BYTE    "Enter second number ",0
-_sum			DWORD	 ?
-first_num		DWORD	 ?
-second_num		DWORD	 ?
-_char			DWORD	 " + "
-_equal			DWORD	 " = "
-_minus			DWORD	 " - "
-_multiply		DWORD	 " * "
-_division		DWORD	 " / "
-
-.data?
-_any QWORD 5000 DUP(?)
-
-; (insert variable definitions here)
-
+	; declare variables here
+	name		WORD		DUP(?)
+	_title		BYTE		"Composite Numbers		programmed by Harshvardhan",0
+	info1		BYTE		"Enter the number of composite numbers you would like to see.",0
+	info2		BYTE		"I accept orders for upto 400 composites ",0
+	question	BYTE		"Enter the number of composites to display [1-400]: ",0
+	error		BYTE		"Out of range. Try again.",0
+	empty		BYTE		"Number has no composites",0
+	range		DWORD		?
+	composites  DWORD		?
+	jack		DWORD		?
+	spaces		BYTE		"   ",0
+	lines		DWORD		?
 .code
-main PROC
+main proc
+	; write your code here
 
-mov		edx, OFFSET	header
-call	WriteString
-call	crlf
-call	crlf
-;mov		edx, OFFSET	First_num
-;call	ReadInt
-mov		edx, OFFSET _instructions
-call	WriteString
-call	crlf
-call	crlf
+	CALL introduction
+	CALL GetUserData
+	CALL ShowComposites
 
-mov		edx, OFFSET str1
-call	WriteString
-call	ReadInt
-mov		first_num, eax
-
-mov		edx, OFFSET str2
-call	WriteString
-call	ReadInt
-mov		second_num, eax
-call	crlf
-
-;					Code for Addition
-mov eax,first_num 
-add eax,second_num
-mov ebx, first_num
-mov _sum, eax
-mov eax,first_num
-call WriteDec
-mov edx, OFFSET _char
-;mov edx, _char
-Call WriteString
-mov eax,second_num
-call WriteDec
- mov edx, OFFSET _equal
-call WriteString
-mov eax,_sum
-call WriteDec
-call crlf
-
-;					Code for subtraction
-mov eax,first_num 
-sub eax,second_num
-mov ebx, first_num
-mov _sum, eax
-mov eax,first_num
-call WriteDec
-mov edx, OFFSET _minus
-;mov edx, _char
-Call WriteString
-mov eax,second_num
-call WriteDec
- mov edx, OFFSET _equal
-call WriteString
-mov eax,_sum
-call WriteDec
-call crlf
-
-
-;					Code for Multiplication
-mov eax,first_num 
-imul eax,second_num
-mov ebx, first_num
-mov _sum, eax
-mov eax,first_num
-call WriteDec
-mov edx, OFFSET _multiply
-;mov edx, _char
-Call WriteString
-mov eax,second_num
-call WriteDec
- mov edx, OFFSET _equal
-call WriteString
-mov eax,_sum
-call WriteDec; (insert executable instructions here)
-call crlf
-
-;					Code for division
-mov edx,0					; Stores the remainder, make sure it is empty
-mov eax,first_num			; is the dividend
-mov ecx,second_num			; is the divisor
-div ecx						; Div operator Pg 617 TB (788 pdf)
-mov ebx, eax
-mov _sum, eax
-mov eax,first_num
-call WriteDec
-mov _sum,edx
-mov edx, OFFSET _division
-;mov edx, _division
-Call WriteString
-mov eax,second_num
-call WriteDec
- mov edx, OFFSET _equal
-call WriteString
-mov eax,ebx
-call WriteDec
-.data
-temp BYTE " remainder ",0
-.code
-mov edx,OFFSET temp
-call WriteString
-mov eax,_sum
-call WriteDec
-call crlf
-
-
-	exit	; exit to operating system
+	invoke ExitProcess,0
 main ENDP
 
-; (insert additional procedures here)
+;---------------------------------------------------------------------------------------------------------
+;Introduction
+;Prints out the description and rules for the programs
+;---------------------------------------------------------------------------------------------------------
 
-END main
+introduction proc
+	mov edx, OFFSET _title
+	CALL WriteString
+	CALL CRLF
+	CALL CRLF
+	mov edx, OFFset info1
+	CALL WriteString
+	CALL CRLF
+	mov edx, OFFset info2
+	CALL WriteString
+	CALL CRLF
+	CALL CRLF
+	ret
+
+introduction ENDP
+
+;---------------------------------------------------------------------------------------------------------
+;GetUserData
+;Takes in data from the user and checks wether the number is in the correct range
+;
+;---------------------------------------------------------------------------------------------------------
+
+GetUserData proc
+	
+
+	mov edx, OFFSET question
+	CALL WriteString
+	CALL ReadInt
+	mov range,eax
+	CALL CRLF
+
+	validate:
+		cmp eax,0
+		jle errorL
+		cmp eax,400
+		jg errorL
+		ret
+
+	errorL:
+		mov edx, OFFSET error
+		CALL WriteString
+		call crlf
+		jmp getuserdata
+
+GetUserData ENDP
+
+;---------------------------------------------------------------------------------------------------------
+;ShowComposites
+;Calculates and shows all the composite numbers one by one
+;---------------------------------------------------------------------------------------------------------
+
+ShowComposites PROC
+	mov lines,0
+	mov jack,eax				; Keeps a copy of eax
+
+	cmp eax,3
+	jle _nocomp					; checks if number entered is <= 3
+
+	mov ecx,eax					; Sets the loop counter
+	sub ecx,3					; Removes numbers 1,2,3 as these are handles seperately
+	mov eax,4					; the first composite number to be checked
+	mov ebx,2					; first divisor
+	jmp iscomposite
+
+	_nocomp:
+		mov edx, OFFSET empty
+		CALL WriteString
+		jmp place_holder
+
+	iscomposite:
+		mov ebx,2
+		
+		mov jack,eax
+		jmp has_factors			; Check if composite 
+		
+		p_holder1:
+			add eax,1
+		loop iscomposite
+		
+	jmp place_holder
+	
+	has_factors:
+		mov edx,0
+		push eax			;PUSH OPERATION PERFORMED
+		div ebx
+		cmp edx,0		; Check if the remainder is 0 (0 = factor)
+		je done
+		pop eax				;POP
+		add ebx,1
+		cmp eax,ebx
+		jle p_holder1
+		jmp has_factors
+
+	done:
+		cmp lines,10
+		jne _ignorelinespace
+		mov lines,0
+		CALL CRLF
+		_ignorelinespace:
+			pop eax
+			Call WriteDec
+		mov edx,OFFSET spaces
+		CALL WriteString
+		;add print statement
+		add lines,1
+		jmp p_holder1
+		
+	place_holder:
+		ret
+
+ShowComposites ENDP
+
+end main
